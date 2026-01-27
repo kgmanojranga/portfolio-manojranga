362832290b74b911b14f533802b6047e3c720172
kgmanojranga <manojranga7583@gmail.com>
2026-01-28 01:11:13 +0530

    chore: update HomePage.tsx

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,14 +1,11 @@
-import HeroSection from '../components/sections/HeroSection';
 import Interactive3DWave from '../components/common/Interactive3DWave.tsx';
 import AboutPage from './AboutPage.tsx';
-import HeroSectionCopy from '../components/sections/HeroSectionCopy.tsx';
+import HeroSection from '../components/sections/HeroSection.tsx';
 
 const HomePage = () => {
   return (
     <>
       {<Interactive3DWave />}
-      {/*<ScrollAnimatedHero />*/}
-      <HeroSectionCopy />
       <HeroSection />
       <AboutPage />
       {/* Add more sections below that appear on scroll */}
