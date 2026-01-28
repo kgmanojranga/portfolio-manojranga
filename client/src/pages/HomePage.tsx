1fbc77c5b265ec5fc11d5d7a34b472a12942ba79
kgmanojranga <manojranga7583@gmail.com>
2026-01-29 00:07:15 +0530

    chore: add about section to home page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,17 +1,11 @@
-import Interactive3DWave from '../components/common/Interactive3DWave.tsx';
-import AboutPage from './AboutPage.tsx';
 import HeroSection from '../components/sections/HeroSection.tsx';
+import AboutSection from '../components/sections/AboutSection.tsx';
 
 const HomePage = () => {
   return (
     <>
-      {<Interactive3DWave />}
       <HeroSection />
-      <AboutPage />
-      {/* Add more sections below that appear on scroll */}
-      {/*<section className="min-h-screen">*/}
-      {/*  Other content here*/}
-      {/*</section>*/}
+      <AboutSection />
     </>
   );
 };
