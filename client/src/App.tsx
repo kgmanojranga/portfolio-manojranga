65febd9a0fdca320e694dbeb794b139f6daf863e
kgmanojranga <manojranga7583@gmail.com>
2026-02-13 14:32:28 +0530

    feat: add vercel analytics to the project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import { router } from './router';
 import './App.css';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
